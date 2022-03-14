d4bf735256fbb33d148d3b51cd7aaaa1131380ef
Dimas Kurniawan <62171509+dimas519@users.noreply.github.com>
2022-03-14 21:15:44 +0700

    first init

---
+++ main.py
@@ -0,0 +1,14 @@
+from Input import Input
+from Tokenizing import Tokenizing
+
+
+#untuk debug
+import os
+os.system('cls||clear')
+
+input=Input()
+arrInput=input()
+
+Token=Tokenizing(arrInput)
+Token()
+
